2d4886c43c01a795f7491f4f48806930f301e012
Arvin Samuel A <140853028+Arvin-Samuel-A@users.noreply.github.com>
2024-02-15 21:04:45 +0530

    Update and rename Area & Circumference of Circle.py to Basics/Area & Circumference of Circle.py

--- Basics/Area & Circumference of Circle.py
+++ Basics/Area & Circumference of Circle.py
@@ -3,4 +3,4 @@ R=float(input("Enter the value of the Radius(in cms) :"))
 A=pi*R**2
 C=2*pi*R
 print("The Area of the Circle with Radius " + str(R)+ " is(in cms^2) =" ,A)
-print("The Circumference of the Circle with Radius " + str(R)+ " is(in cmss) =" ,C)
+print("The Circumference of the Circle with Radius " + str(R)+ " is(in cms) =" ,C)

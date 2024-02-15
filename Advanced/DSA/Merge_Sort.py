d88c9c04bb6ff3a9f550fdc46a38f56b9617210c
Arvin Samuel A. <140853028+Arvin-Samuel-A@users.noreply.github.com>
2024-02-15 19:28:31 +0000

    Learning

---
+++ Advanced/DSA/Merge_Sort.py
@@ -0,0 +1,56 @@
+def Merge(List1, List2):
+
+    Len1=len(List1)
+    Len2=len(List2)
+
+    List3=[]
+    x=y=z=0
+
+    while (z<Len1+Len2):
+
+        if (x==Len1):
+
+            List3+=List2[y:]
+            z+=(Len2-y)
+
+        elif (y==Len2):
+
+            List3+=List1[x:]
+            z+=(Len2-x)
+
+        elif List1[x]<List2[y]:
+
+            List3+=[List1[x]]
+
+            x+=1
+            z+=1
+
+        else:
+
+            List3+=[List2[y]]
+
+            y+=1
+            z+=1
+
+    return List3
+
+def Merge_Sort(List):
+
+    Len=len(List)
+
+    if Len<=1:
+
+        return List
+    
+    Left=Merge_Sort(List[:Len//2])
+    Right=Merge_Sort(List[Len//2:])
+
+    Result=Merge(Left, Right)
+
+    return Result
+
+List=eval(input("Enter the List of Elements to be Sorted : "))
+
+print()
+
+print("The Sorted List is : ", Merge_Sort(List))
